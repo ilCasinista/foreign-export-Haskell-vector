b060ef4c27db902e77da6e72bcff745181a04b17
myself <noreply@mario.com>
2020-04-19 17:43:53 +0200

    Add StablePtr_Massiv.

---
+++ StablePtr_Massiv/DoubleVector_C.h
@@ -0,0 +1,9 @@
+#ifndef DOUBLEVECTOR_C
+#define DOUBLEVECTOR_C
+
+#include "HsFFI.h"
+#include "../DoubleVector.h"
+
+typedef C_DoubleVector C_DoubleVector_C;
+
+#endif
---
+++ StablePtr_Massiv/Lib.hs
@@ -0,0 +1,27 @@
+module StablePtr_Massiv.Lib (twiceVec, free) where
+
+import           Foreign
+import           Foreign.C.Types
+import           Foreign.StablePtr
+import           StorableInstances
+import qualified Data.Massiv.Array        as Massiv
+import qualified Data.Massiv.Array.Unsafe as Massiv
+
+
+foreign export ccall "twiceVec_hs" twiceVec :: CInt -> Ptr CDouble -> Ptr CInt -> Ptr (Ptr CDouble) -> IO (StablePtr (Massiv.Array Massiv.S Massiv.Ix1 CDouble))
+
+foreign export ccall "massiv_free_hs" freeStablePtr :: StablePtr (Massiv.Array Massiv.S Massiv.Ix1 CDouble) -> IO ()
+
+
+ --                                |____________OUPUT__________|
+twiceVec :: CInt -> Ptr CDouble -> Ptr CInt -> Ptr (Ptr CDouble) -> IO (StablePtr (Massiv.Array Massiv.S Massiv.Ix1 CDouble))
+twiceVec csz srcPtr   czPtr dstPtrPtr = do
+  srcArr <- flip (Massiv.unsafeArrayFromForeignPtr0 Massiv.Par) (Massiv.Sz $ fromIntegral csz) <$> newForeignPtr_ srcPtr
+  dstArr <- Massiv.computeIO (Massiv.map (*2) srcArr)
+
+  dstArr_MUTABLE <- Massiv.thaw dstArr -- Should I use freeze somewhere?
+
+  poke czPtr (fromIntegral . Massiv.unSz . Massiv.size $ dstArr)
+  Massiv.withPtr dstArr_MUTABLE $ poke dstPtrPtr
+
+  newStablePtr dstArr
